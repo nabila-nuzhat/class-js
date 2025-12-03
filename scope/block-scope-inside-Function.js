0b024b0db259a40ed4ae18700f653bdb34302b47
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2025-12-03 11:15:01 +0600

    block scope inside function example added

--- scope/block-scope-inside-Function.js
+++ scope/block-scope-inside-Function.js
@@ -1,8 +1,10 @@
 function myFunction() {
   if (true) {
     var localBlockVariableVar = "I'm in local-block scope VAR";
-    // let localBlockVariableLet = "I'm also in local-block scope LET";
+    let localBlockVariableLet = "I'm also in local-block scope LET"; // not accessible outside block scope
   }
   console.log(localBlockVariableVar); // Accessible
-  // console.log(localBlockVariableLet); // Error: blockVariable is not defined
+  // console.log(localBlockVariableLet); // Error: ReferenceError: localBlockVariableLet is not defined
 }
+
+myFunction();
