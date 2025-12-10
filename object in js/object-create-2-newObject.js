693216ec414cf4351e816e370e776aa4aaa5a7f2
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2025-12-10 12:42:39 +0600

    new Object() method : object create example added

---
+++ object in js/object-create-2-newObject.js
@@ -0,0 +1,10 @@
+// new Object() method: the built-in Object constructor.
+// Creating new object way
+const compactSuv = new Object(); // new word, Object Constructor
+compactSuv.type = "Hyundai Creta";
+compactSuv.model = "Grand";
+compactSuv.color = "Robust Emerald Pearl";
+compactSuv["Engine Type"] = "1.5L Turbo Petrol (around 158-160 bhp"
+console.log(compactSuv);
+
+
