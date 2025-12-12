1ea6a210af7e7629a70ed0d61ccac63a419015b9
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2025-12-12 12:20:00 +0600

    array.flat() method added in Obeject.keys() example

--- object in js/create-array of Object-keys-values-entries.js
+++ object in js/create-array of Object-keys-values-entries.js
@@ -1,6 +1,6 @@
 const objectName = {
     key1 : "value 1",
-    key2 : "value 2",
+    key2 : 2,
     key3 : "value 3",
     "key 4" : "value 4"
 }
@@ -12,5 +12,6 @@ let array3 = Object.entries(objectName);
 console.log(array1);
 console.log(array2);
 console.log("Array 3: \n",array3);
+console.log("array.flat() \n",array3.flat());// flat makes the nested array into 1 full array
 
 
