9e4ccd5cb160583778904641f547d7ecddf6cd46
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2025-12-13 20:02:02 +0600

    object.entries() method example added

---
+++ object method/object-entries.js
@@ -0,0 +1,26 @@
+// Object.entries() method ------
+function Tenants(rent, name, age, members){
+    this.rent = rent;
+    this.name = name;
+    this.age = age;
+    this.familyMembers = members;
+}
+
+const flatA = new Tenants(10000, "ab", 35, 4); // desired object-name = "new" keyword is used followed by calling the "Constructor Function" 
+const flatB = new Tenants(12000, "dc", 45, 3);
+
+console.log(flatA);
+console.log(flatB);
+ 
+// Object.entries()
+console.log("Entries Array:\n",Object.entries(flatA));
+/** O/P
+ * Entries Array:
+ [
+  [ 'rent', 10000 ],
+  [ 'name', 'ab' ],
+  [ 'age', 35 ],
+  [ 'familyMembers', 4 ]
+]
+  
+ */
