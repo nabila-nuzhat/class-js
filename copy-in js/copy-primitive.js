ec86a415113e708d4fbe50fb446d2bb2df71d26c
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2025-12-25 11:31:52 +0600

    copy-non primitive data example edited

--- copy-in js/copy-primitive.js
+++ copy-in js/copy-primitive.js
@@ -1,4 +1,8 @@
 let a = 11;
 let b = a;
+console.log(a===b);
 a = 20;
 console.log(b); // 11, bcz primitive data type's value is saved in "stack memory". Change in "a" variable value will not change value of "b" variable
+
+console.log(a===b);
+
