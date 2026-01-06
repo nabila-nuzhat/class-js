4a86cd87a62b64d398309b9d61c46c7a64f1979d
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-01-06 11:36:25 +0600

    dom style change added

---
+++ dom/change-dom.js
@@ -0,0 +1,21 @@
+// change DOM ================
+// innerHTML -----------------------------
+
+document.querySelector("h4").innerHTML = "My Name is Samantha";
+const h4Elm = document.querySelector("h4");
+h4Elm.innerHTML = "My Name is Nilima"
+
+// -------------------- CSS add ----------------------------
+// CSS JS > CSS inline --------------------
+// elementvariable.style.other-property ---------------
+h4Elm.style.color = "#eb4034";
+h4Elm.style.backgroundColor = "rgb(252, 214, 144)";
+
+// button: style change by click ----------------
+function styleChange(){
+    h4Elm.style.background = "#32f0ca";
+    h4Elm.style.textAlign = "center";
+    h4Elm.style.fontSize = "30px";
+}
+
+
