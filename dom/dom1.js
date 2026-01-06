623d6f89acb899bef555cd5a782d37f5e390868c
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-01-06 10:50:15 +0600

    ways to access DOM parts added in dom1

--- dom/dom1.js
+++ dom/dom1.js
@@ -1,3 +1,32 @@
 // accesses HTML each parts as objects 
 // eg. document.body ...etc
 console.log(document.title);
+console.log(document);
+
+
+// Ways to access DOM parts ==================================================================
+console.log(document.getElementById("para"));
+console.log(document.getElementsByClassName("pn")); // HTML Collection= looks like "array", but NOT array
+
+console.log( document.getElementsByTagName("h1")); // HTML Collection [h1]
+
+// ----------- Ways to access DOM parts using Global querySelectorAll / querySelector
+
+const idP = document.querySelector("#para");
+console.log(idP);
+
+const classPn = document.querySelector(".pn"); // <p id="para">khsahdjsckdznc djlsakj</p>
+console.log(classPn); // <div class="pn">11111111111111</div>
+
+
+const divClassPn = document.querySelectorAll(".pn"); 
+console.log(divClassPn); // Nodelist = looks like "array", but NOT array but can be used "forEach()" "array-method" to loop. can be accessed as array using index
+
+// forEach in Nodelist -------------------------------------------------
+divClassPn.forEach((divSingle) => console.log(divSingle) 
+) // all divs
+console.log(divClassPn[1]); // access as index, not array but can be accessed as array using index
+
+
+
+
