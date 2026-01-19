401043810705b3b14bcf40f24e6b6a8742ecadb1
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-01-19 11:21:33 +0600

    event listener theory added

---
+++ dom/THEORY-Dom/event-listener.js
@@ -0,0 +1,16 @@
+// Event Listener **=======================
+// Event: A signal sent by browser to JS
+// * tab open, mouse move, keyboard press, click on white spaces of web page, mouse on which position & pixel, microphone access, video access etc.
+// add.eventListener
+// Syntax (commonly used): element.addEventListener("event name", function(){});
+    // this "function()" provides an "event object" as "parameter"
+    // this "event object" is commonly denoted in shortcut as "e" in the parameter
+    // "e" can be denoted with any other name also, but commonly "e" is used.
+// event in HTML: 
+    // pros: 
+    // cons: overrindiong html, access multiple pages of html
+
+// add Event listener: usually added by JS:
+    //  btnElm.addEventListener("click", function(){
+    // console.log("clicked!!")
+    // });
