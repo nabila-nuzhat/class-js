b8e409f75549f79b60553d7fd05cb35235dcf3af
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-02-09 11:31:42 +0600

    * all import: namespace objcet import + default import added

--- module/module-3.js
+++ module/module-3.js
@@ -1,5 +1,16 @@
+// to check * all import, imported as a object =====================
+    //  -  namespace export object, does NOT include the "default" export.
+    // - namespace export object ONLY contains  named exports
 
-// similar name function existing in one file =================
+import multiply, * as module4 from "./module-4.js" //multiply is default
+
+// similar name function existing in one file =========================
     export function add(c, d){
         return 1000* c + d;
     }
+// * all import, all variables, arrow function-variable, function declaration imported as a single object ===============================
+    console.log(module4.p);
+    console.log(module4.subtract2(2, 3)); // variable of arrow-function, imported as a object module4 that's why dot notation //28
+    console.log(multiply(1, 5)); // function declaration // 100
+
+    
---
+++ module/module-4.js
@@ -0,0 +1,6 @@
+export const subtract2 = (a, b) => a - 10* b;
+export default function multiply(a, b) {
+    //  let n = a*b ;
+     return a*b*20;
+} //
+export const p = 80.88;
