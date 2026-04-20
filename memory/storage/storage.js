25e56d6172a7f4a1bbc8e5a21e5e234d80e65a63
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-04-20 20:50:35 +0600

    storage concept added

---
+++ memory/storage/storage.js
@@ -0,0 +1,14 @@
+// session Storage
+sessionStorage.setItem("draft", "hello, creating session storage");
+// localStorage
+localStorage.setItem("draft2", "hi, creating Local storage");
+localStorage.setItem("draft", "HOLAA World");
+localStorage.setItem("draft Number", 1200);
+
+
+// to keep in local storage Object use JSON.stringify
+// localStorage.setItem("draft", JSON.stringify())
+
+
+// remove from local storage:
+localStorage.removeItem("draft2", "hi, creating Local storage");
